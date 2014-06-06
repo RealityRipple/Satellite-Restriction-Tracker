02cd4553e04dbc7695843b64c24399aa9ba57a0b
Andy <webmaster@RealityRipple.com>
2014-06-06 13:42:17 -0700

    Re-set version numbers after testing Update form.

--- RestrictionTracker/My Project/AssemblyInfo.vb
+++ RestrictionTracker/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.3.1.11")> 
-<Assembly: AssemblyFileVersion("0.3.1.11")> 
+<Assembly: AssemblyVersion("0.3.2")> 
+<Assembly: AssemblyFileVersion("0.3.2")> 
