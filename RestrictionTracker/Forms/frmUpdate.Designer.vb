bbab8cca1140b275218ede7887d23550731477c6
Andy <webmaster@RealityRipple.com>
2014-06-10 14:03:17 -0700

    Improved Update dialog style.

--- RestrictionTracker/Forms/frmUpdate.Designer.vb
+++ RestrictionTracker/Forms/frmUpdate.Designer.vb
@@ -22,14 +22,17 @@ Partial Class frmUpdate
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+    Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmUpdate))
     Me.pnlUpdate = New System.Windows.Forms.TableLayoutPanel()
-    Me.lblNewVer = New System.Windows.Forms.Label()
+    Me.lblTitle = New System.Windows.Forms.Label()
     Me.txtInfo = New System.Windows.Forms.TextBox()
     Me.lblBETA = New System.Windows.Forms.Label()
     Me.pnlButtons = New System.Windows.Forms.TableLayoutPanel()
     Me.cmdDownload = New System.Windows.Forms.Button()
     Me.cmdCancel = New System.Windows.Forms.Button()
+    Me.cmdChanges = New System.Windows.Forms.Button()
     Me.chkStopBETA = New System.Windows.Forms.CheckBox()
+    Me.lblNewVer = New System.Windows.Forms.Label()
     Me.pnlUpdate.SuspendLayout()
     Me.pnlButtons.SuspendLayout()
     Me.SuspendLayout()
@@ -40,57 +43,62 @@ Partial Class frmUpdate
     Me.pnlUpdate.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
     Me.pnlUpdate.ColumnCount = 1
     Me.pnlUpdate.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
-    Me.pnlUpdate.Controls.Add(Me.lblNewVer, 0, 0)
-    Me.pnlUpdate.Controls.Add(Me.txtInfo, 0, 1)
+    Me.pnlUpdate.Controls.Add(Me.lblTitle, 0, 0)
+    Me.pnlUpdate.Controls.Add(Me.txtInfo, 0, 5)
     Me.pnlUpdate.Controls.Add(Me.lblBETA, 0, 2)
     Me.pnlUpdate.Controls.Add(Me.pnlButtons, 0, 4)
     Me.pnlUpdate.Controls.Add(Me.chkStopBETA, 0, 3)
+    Me.pnlUpdate.Controls.Add(Me.lblNewVer, 0, 1)
     Me.pnlUpdate.Dock = System.Windows.Forms.DockStyle.Fill
     Me.pnlUpdate.Location = New System.Drawing.Point(0, 0)
     Me.pnlUpdate.Name = "pnlUpdate"
-    Me.pnlUpdate.RowCount = 5
+    Me.pnlUpdate.RowCount = 6
+    Me.pnlUpdate.RowStyles.Add(New System.Windows.Forms.RowStyle())
+    Me.pnlUpdate.RowStyles.Add(New System.Windows.Forms.RowStyle())
+    Me.pnlUpdate.RowStyles.Add(New System.Windows.Forms.RowStyle())
+    Me.pnlUpdate.RowStyles.Add(New System.Windows.Forms.RowStyle())
     Me.pnlUpdate.RowStyles.Add(New System.Windows.Forms.RowStyle())
     Me.pnlUpdate.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
-    Me.pnlUpdate.RowStyles.Add(New System.Windows.Forms.RowStyle())
-    Me.pnlUpdate.RowStyles.Add(New System.Windows.Forms.RowStyle())
-    Me.pnlUpdate.RowStyles.Add(New System.Windows.Forms.RowStyle())
-    Me.pnlUpdate.Size = New System.Drawing.Size(276, 332)
-    Me.pnlUpdate.TabIndex = 0
+    Me.pnlUpdate.Size = New System.Drawing.Size(297, 277)
+    Me.pnlUpdate.TabIndex = 2
     '
-    'lblNewVer
+    'lblTitle
     '
-    Me.lblNewVer.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-    Me.lblNewVer.AutoSize = True
-    Me.lblNewVer.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-    Me.lblNewVer.Location = New System.Drawing.Point(3, 0)
-    Me.lblNewVer.Name = "lblNewVer"
-    Me.lblNewVer.Size = New System.Drawing.Size(270, 40)
-    Me.lblNewVer.TabIndex = 0
-    Me.lblNewVer.Text = "Satellite Restriction Tracker Update" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Version 0.0.0.0"
-    Me.lblNewVer.TextAlign = System.Drawing.ContentAlignment.TopCenter
+    Me.lblTitle.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+    Me.lblTitle.AutoSize = True
+    Me.lblTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+    Me.lblTitle.Location = New System.Drawing.Point(3, 6)
+    Me.lblTitle.Margin = New System.Windows.Forms.Padding(3, 6, 3, 6)
+    Me.lblTitle.Name = "lblTitle"
+    Me.lblTitle.Size = New System.Drawing.Size(291, 48)
+    Me.lblTitle.TabIndex = 0
+    Me.lblTitle.Text = "Satellite Restriction Tracker Update"
     '
     'txtInfo
     '
-    Me.txtInfo.Dock = System.Windows.Forms.DockStyle.Fill
-    Me.txtInfo.Location = New System.Drawing.Point(3, 43)
+    Me.txtInfo.Dock = System.Windows.Forms.DockStyle.Top
+    Me.txtInfo.Location = New System.Drawing.Point(3, 291)
     Me.txtInfo.Multiline = True
     Me.txtInfo.Name = "txtInfo"
     Me.txtInfo.ReadOnly = True
     Me.txtInfo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
-    Me.txtInfo.Size = New System.Drawing.Size(270, 179)
-    Me.txtInfo.TabIndex = 1
+    Me.txtInfo.Size = New System.Drawing.Size(291, 1)
+    Me.txtInfo.TabIndex = 3
+    Me.txtInfo.Visible = False
     '
     'lblBETA
     '
     Me.lblBETA.AutoSize = True
     Me.lblBETA.ForeColor = System.Drawing.Color.Firebrick
-    Me.lblBETA.Location = New System.Drawing.Point(3, 225)
+    Me.lblBETA.Location = New System.Drawing.Point(3, 165)
+    Me.lblBETA.Margin = New System.Windows.Forms.Padding(3)
     Me.lblBETA.Name = "lblBETA"
-    Me.lblBETA.Size = New System.Drawing.Size(245, 52)
-    Me.lblBETA.TabIndex = 2
-    Me.lblBETA.Text = "BETA updates may have bugs and other issues" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "that haven't been worked out yet, bu" & _
-    "t need testing" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "on a wide range of accounts." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Please back up your history before" & _
-    " using BETAs."
+    Me.lblBETA.Size = New System.Drawing.Size(290, 65)
+    Me.lblBETA.TabIndex = 4
+    Me.lblBETA.Text = "BETA updates may have bugs and other issues that haven't been" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "worked out yet, bu" & _
+    "t need testing on a wide range of accounts." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Please back up your history before " & _
+    "using BETAs."
+    Me.lblBETA.Visible = False
     '
     'pnlButtons
     '
@@ -98,63 +106,89 @@ Partial Class frmUpdate
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
     Me.pnlButtons.AutoSize = True
     Me.pnlButtons.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
-    Me.pnlButtons.ColumnCount = 2
-    Me.pnlButtons.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
-    Me.pnlButtons.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
-    Me.pnlButtons.Controls.Add(Me.cmdDownload, 0, 0)
-    Me.pnlButtons.Controls.Add(Me.cmdCancel, 1, 0)
-    Me.pnlButtons.Location = New System.Drawing.Point(0, 301)
+    Me.pnlButtons.ColumnCount = 4
+    Me.pnlButtons.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+    Me.pnlButtons.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
+    Me.pnlButtons.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
+    Me.pnlButtons.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
+    Me.pnlButtons.Controls.Add(Me.cmdDownload, 1, 0)
+    Me.pnlButtons.Controls.Add(Me.cmdCancel, 2, 0)
+    Me.pnlButtons.Controls.Add(Me.cmdChanges, 3, 0)
+    Me.pnlButtons.Location = New System.Drawing.Point(0, 257)
     Me.pnlButtons.Margin = New System.Windows.Forms.Padding(0)
     Me.pnlButtons.Name = "pnlButtons"
     Me.pnlButtons.RowCount = 1
-    Me.pnlButtons.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
-    Me.pnlButtons.Size = New System.Drawing.Size(276, 31)
-    Me.pnlButtons.TabIndex = 3
+    Me.pnlButtons.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+    Me.pnlButtons.Size = New System.Drawing.Size(297, 31)
+    Me.pnlButtons.TabIndex = 2
     '
     'cmdDownload
     '
+    Me.cmdDownload.AutoSize = True
     Me.cmdDownload.Dock = System.Windows.Forms.DockStyle.Fill
     Me.cmdDownload.FlatStyle = System.Windows.Forms.FlatStyle.System
-    Me.cmdDownload.Location = New System.Drawing.Point(3, 3)
+    Me.cmdDownload.Location = New System.Drawing.Point(2, 3)
     Me.cmdDownload.Name = "cmdDownload"
-    Me.cmdDownload.Size = New System.Drawing.Size(132, 25)
+    Me.cmdDownload.Size = New System.Drawing.Size(120, 25)
     Me.cmdDownload.TabIndex = 0
     Me.cmdDownload.Text = "Download Update"
     Me.cmdDownload.UseVisualStyleBackColor = True
     '
     'cmdCancel
     '
+    Me.cmdCancel.AutoSize = True
     Me.cmdCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel
     Me.cmdCancel.Dock = System.Windows.Forms.DockStyle.Fill
     Me.cmdCancel.FlatStyle = System.Windows.Forms.FlatStyle.System
-    Me.cmdCancel.Location = New System.Drawing.Point(141, 3)
+    Me.cmdCancel.Location = New System.Drawing.Point(128, 3)
     Me.cmdCancel.Name = "cmdCancel"
-    Me.cmdCancel.Size = New System.Drawing.Size(132, 25)
+    Me.cmdCancel.Size = New System.Drawing.Size(80, 25)
     Me.cmdCancel.TabIndex = 1
     Me.cmdCancel.Text = "Not Now"
     Me.cmdCancel.UseVisualStyleBackColor = True
+    '
+    'cmdChanges
+    '
+    Me.cmdChanges.AutoSize = True
+    Me.cmdChanges.FlatStyle = System.Windows.Forms.FlatStyle.System
+    Me.cmdChanges.Location = New System.Drawing.Point(214, 3)
+    Me.cmdChanges.Name = "cmdChanges"
+    Me.cmdChanges.Size = New System.Drawing.Size(80, 25)
+    Me.cmdChanges.TabIndex = 2
+    Me.cmdChanges.Text = "Changes >>"
+    Me.cmdChanges.UseVisualStyleBackColor = True
     '
     'chkStopBETA
     '
     Me.chkStopBETA.AutoSize = True
     Me.chkStopBETA.FlatStyle = System.Windows.Forms.FlatStyle.System
-    Me.chkStopBETA.Location = New System.Drawing.Point(6, 280)
+    Me.chkStopBETA.Location = New System.Drawing.Point(6, 236)
     Me.chkStopBETA.Margin = New System.Windows.Forms.Padding(6, 3, 3, 3)
     Me.chkStopBETA.Name = "chkStopBETA"
     Me.chkStopBETA.Size = New System.Drawing.Size(189, 18)
-    Me.chkStopBETA.TabIndex = 4
+    Me.chkStopBETA.TabIndex = 5
     Me.chkStopBETA.Text = "Don't notify me of BETA updates."
     Me.chkStopBETA.UseVisualStyleBackColor = True
+    Me.chkStopBETA.Visible = False
+    '
+    'lblNewVer
+    '
+    Me.lblNewVer.AutoSize = True
+    Me.lblNewVer.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+    Me.lblNewVer.Location = New System.Drawing.Point(3, 66)
+    Me.lblNewVer.Margin = New System.Windows.Forms.Padding(3, 6, 3, 6)
+    Me.lblNewVer.Name = "lblNewVer"
+    Me.lblNewVer.Size = New System.Drawing.Size(280, 90)
+    Me.lblNewVer.TabIndex = 1
+    Me.lblNewVer.Text = resources.GetString("lblNewVer.Text")
     '
     'frmUpdate
     '
-    Me.AcceptButton = Me.cmdDownload
     Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
     Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
     Me.AutoSize = True
     Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
-    Me.CancelButton = Me.cmdCancel
-    Me.ClientSize = New System.Drawing.Size(276, 332)
+    Me.ClientSize = New System.Drawing.Size(297, 277)
     Me.Controls.Add(Me.pnlUpdate)
     Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
     Me.Icon = Global.RestrictionTracker.My.Resources.Resources.sat
@@ -168,16 +202,19 @@ Partial Class frmUpdate
     Me.pnlUpdate.ResumeLayout(False)
     Me.pnlUpdate.PerformLayout()
     Me.pnlButtons.ResumeLayout(False)
+    Me.pnlButtons.PerformLayout()
     Me.ResumeLayout(False)
     Me.PerformLayout()
 
   End Sub
   Friend WithEvents pnlUpdate As System.Windows.Forms.TableLayoutPanel
-  Friend WithEvents lblNewVer As System.Windows.Forms.Label
+  Friend WithEvents lblTitle As System.Windows.Forms.Label
   Friend WithEvents txtInfo As System.Windows.Forms.TextBox
   Friend WithEvents lblBETA As System.Windows.Forms.Label
   Friend WithEvents pnlButtons As System.Windows.Forms.TableLayoutPanel
   Friend WithEvents cmdDownload As System.Windows.Forms.Button
   Friend WithEvents cmdCancel As System.Windows.Forms.Button
+  Friend WithEvents cmdChanges As System.Windows.Forms.Button
   Friend WithEvents chkStopBETA As System.Windows.Forms.CheckBox
+  Friend WithEvents lblNewVer As System.Windows.Forms.Label
 End Class
--- RestrictionTracker/Forms/frmUpdate.vb
+++ RestrictionTracker/Forms/frmUpdate.vb
@@ -3,12 +3,6 @@
   Private Ret As Boolean
   Private Sub frmUpdate_Shown(sender As Object, e As System.EventArgs) Handles Me.Shown
     Ret = False
-    sckVerInfo = New CookieAwareWebClient
-    If lblBETA.Visible Then
-      sckVerInfo.DownloadStringAsync(New Uri("http://update.realityripple.com/Satellite_Restriction_Tracker/infob"))
-    Else
-      sckVerInfo.DownloadStringAsync(New Uri("http://update.realityripple.com/Satellite_Restriction_Tracker/info"))
-    End If
   End Sub
   Private Sub frmUpdate_FormClosing(sender As Object, e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
     If sckVerInfo IsNot Nothing Then
@@ -20,15 +14,17 @@
   Public Sub NewUpdate(Version As String, BETA As Boolean)
     If BETA Then
       Me.Text = "New BETA Version Available"
+      lblTitle.Text = Application.ProductName & " BETA Update"
     Else
       Me.Text = "New Version Available"
+      lblTitle.Text = Application.ProductName & " Update"
     End If
-    lblNewVer.Text = Application.ProductName & " Update" & vbNewLine & "Version " & DisplayVersion(Version)
+    lblNewVer.Text = lblNewVer.Text.Replace("%v", DisplayVersion(Version))
     txtInfo.Text = "Loading Update Information" & vbNewLine & vbNewLine & "Please Wait..."
     lblBETA.Visible = BETA
     chkStopBETA.Visible = BETA
   End Sub
-  Private Sub cmdDownload_Click(sender As System.Object, e As System.EventArgs) Handles cmdDownload.Click
+  Private Sub cmdDownload_Click(sender As System.Object, e As System.EventArgs)
     If chkStopBETA.Visible And chkStopBETA.Checked Then
       Me.DialogResult = Windows.Forms.DialogResult.OK
     Else
@@ -37,7 +33,7 @@
     Ret = True
     Me.Close()
   End Sub
-  Private Sub cmdCancel_Click(sender As System.Object, e As System.EventArgs) Handles cmdCancel.Click
+  Private Sub cmdCancel_Click(sender As System.Object, e As System.EventArgs)
     If chkStopBETA.Visible And chkStopBETA.Checked Then
       Me.DialogResult = Windows.Forms.DialogResult.Cancel
     Else
@@ -46,24 +42,62 @@
     Ret = True
     Me.Close()
   End Sub
-  Private Sub sckVerInfo_DownloadStringCompleted(sender As Object, e As System.Net.DownloadStringCompletedEventArgs) Handles sckVerInfo.DownloadStringCompleted
-    If e.Cancelled Then
-      txtInfo.Text = "Info Request Cancelled"
-    ElseIf e.Error IsNot Nothing Then
-      txtInfo.Text = "Info Request Error" & vbNewLine & e.Error.Message
+  Private Sub cmdChanges_Click(sender As System.Object, e As System.EventArgs) Handles cmdChanges.Click
+    If txtInfo.Visible Then
+      cmdChanges.Text = "Changes >>"
+      txtInfo.Visible = False
     Else
-      txtInfo.Text = e.Result
-    End If
-    If sckVerInfo IsNot Nothing Then
-      sckVerInfo.Dispose()
-      sckVerInfo = Nothing
+      txtInfo.Height = 100
+      cmdChanges.Text = "Changes <<"
+      txtInfo.Visible = True
+      If Not txtInfo.Text.StartsWith("Released:") Then
+        cmdDownload.Focus()
+        cmdChanges.Enabled = False
+        txtInfo.Text = "Loading Update Information" & vbNewLine & vbNewLine & "Please Wait..."
+        Dim getInfo As New MethodInvoker(AddressOf GetVerInfo)
+        getInfo.BeginInvoke(Nothing, Nothing)
+      End If
     End If
   End Sub
+  Private Sub GetVerInfo()
+    sckVerInfo = New CookieAwareWebClient
+    If lblBETA.Visible Then
+      sckVerInfo.DownloadStringAsync(New Uri("http://update.realityripple.com/Satellite_Restriction_Tracker/infob"))
+    Else
+      sckVerInfo.DownloadStringAsync(New Uri("http://update.realityripple.com/Satellite_Restriction_Tracker/info"))
+    End If
+  End Sub
+  Private Sub sckVerInfo_DownloadStringCompleted(sender As Object, e As System.Net.DownloadStringCompletedEventArgs) Handles sckVerInfo.DownloadStringCompleted
+    If Me.InvokeRequired Then
+      Me.Invoke(New Net.DownloadStringCompletedEventHandler(AddressOf sckVerInfo_DownloadStringCompleted), sender, e)
+    Else
+      If e.Cancelled Then
+        txtInfo.Text = "Info Request Cancelled"
+      ElseIf e.Error IsNot Nothing Then
+        txtInfo.Text = "Info Request Error" & vbNewLine & e.Error.Message
+      Else
+        txtInfo.Text = e.Result
+      End If
+      If sckVerInfo IsNot Nothing Then
+        sckVerInfo.Dispose()
+        sckVerInfo = Nothing
+      End If
+      cmdChanges.Enabled = True
+      cmdChanges.Focus()
+    End If
+  End Sub
+  Private Delegate Sub FailureHandler(sender As Object, e As CookieAwareWebClient.ErrorEventArgs)
   Private Sub sckVerInfo_Failure(sender As Object, e As CookieAwareWebClient.ErrorEventArgs) Handles sckVerInfo.Failure
-    txtInfo.Text = "Info Request Error" & vbNewLine & e.Error.Message
-    If sckVerInfo IsNot Nothing Then
-      sckVerInfo.Dispose()
-      sckVerInfo = Nothing
+    If Me.InvokeRequired Then
+      Me.Invoke(New FailureHandler(AddressOf sckVerInfo_Failure), sender, e)
+    Else
+      txtInfo.Text = "Info Request Error" & vbNewLine & e.Error.Message
+      If sckVerInfo IsNot Nothing Then
+        sckVerInfo.Dispose()
+        sckVerInfo = Nothing
+      End If
+      cmdChanges.Enabled = True
+      cmdChanges.Focus()
     End If
   End Sub
 End Class
