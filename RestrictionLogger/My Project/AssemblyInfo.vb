1b1d0fe92281b4a29f05d5cd771376b8bf2beebf
Andy <webmaster@RealityRipple.com>
2014-07-30 20:50:39 -0700

    Set version numbers to BETA. I don't yet trust the new Exede code with a final release until I get some more robust checking.

--- RestrictionLogger/My Project/AssemblyInfo.vb
+++ RestrictionLogger/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("0.4.1.01")> 
-<Assembly: AssemblyFileVersion("0.4.1.01")> 
+<Assembly: AssemblyVersion("0.4.1.11")> 
+<Assembly: AssemblyFileVersion("0.4.1.11")> 
