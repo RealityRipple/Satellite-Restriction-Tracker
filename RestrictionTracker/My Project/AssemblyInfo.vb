6dec253f8405155c08367878578bc9f3bb598772
Andy <webmaster@RealityRipple.com>
2014-09-09 00:17:51 -0700

    Upped version numbers.

--- RestrictionTracker/My Project/AssemblyInfo.vb
+++ RestrictionTracker/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.4.4.15")> 
-<Assembly: AssemblyFileVersion("0.4.4.15")> 
+<Assembly: AssemblyVersion("0.4.4.22")> 
+<Assembly: AssemblyFileVersion("0.4.4.22")> 
