9dc67763af0bd896ae049a35e68b71bd36bb3e5e
Andy <webmaster@RealityRipple.com>
2015-05-09 00:54:11 -0700

    Changed the LineBreak control's Paint function to be a little more managed and respect clipping regions.

--- RestrictionTracker/Classes/Visual Elements/clsLineBreak.vb
+++ RestrictionTracker/Classes/Visual Elements/clsLineBreak.vb
@@ -5,7 +5,8 @@ Public Class LineBreak
     Me.TabStop = False
   End Sub
   Private Sub LineBreak_Paint(sender As Object, e As System.Windows.Forms.PaintEventArgs) Handles Me.Paint
-    Using g As Graphics = Graphics.FromHwnd(Me.Handle)
+    Using g As Graphics = e.Graphics
+      g.SetClip(e.ClipRectangle)
       g.DrawLine(New Pen(SystemColors.ButtonShadow, 1), Me.Padding.Left, Me.Padding.Top, Me.DisplayRectangle.Width - (Me.Padding.Right + Me.Padding.Left), Me.Padding.Top)
       g.DrawLine(New Pen(SystemColors.ButtonHighlight, 1), Me.Padding.Left, Me.Padding.Top + 1, Me.DisplayRectangle.Width - (Me.Padding.Right + Me.Padding.Left), Me.Padding.Top + 1)
     End Using
