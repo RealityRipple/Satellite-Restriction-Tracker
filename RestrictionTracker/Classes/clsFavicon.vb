7e22f7752bfb0e42f69c78aef52ee13b25dca31f
Andy <webmaster@RealityRipple.com>
2015-05-09 14:57:01 -0700

    Cleaned up some unused variables and the structure of a couple functions in the Favicon class.

--- RestrictionTracker/Classes/clsFavicon.vb
+++ RestrictionTracker/Classes/clsFavicon.vb
@@ -1,8 +1,6 @@
 ﻿Public Class clsFavicon
   Implements IDisposable
   Private WithEvents wsNetTest As WebClientEx
-  Private c_icon32 As Image
-  Private c_icon16 As Image
   Public Class DownloadIconCompletedEventArgs
     Inherits EventArgs
     Public [Error] As Exception
@@ -16,8 +14,6 @@
   End Class
   Public Event DownloadIconCompleted(sender As Object, e As DownloadIconCompletedEventArgs)
   Public Sub New(URL As String)
-    c_icon16 = Nothing
-    c_icon32 = Nothing
     If String.IsNullOrEmpty(URL) Then Return
     If Not URL.Contains("://") Then URL = "http://" & URL
     ConnectToURL(New Uri(URL), URL)
@@ -60,118 +56,116 @@
     If e.Error IsNot Nothing Then
       If e.UserState Is Nothing Then
         RaiseEvent DownloadIconCompleted(Me, New DownloadIconCompletedEventArgs(Nothing, Nothing, New Exception("Failed to get an icon.")))
-        Return
       Else
         Dim pathURL As New Uri(e.UserState)
         ConnectToFile(New Uri(pathURL.Scheme & "://" & pathURL.Host & "/favicon.ico"), IO.Path.Combine(My.Computer.FileSystem.SpecialDirectories.Temp, "srt_nettest_favicon.ico"))
-        Return
       End If
+      Return
     ElseIf e.Cancelled Then
       Return
+    End If
+    Dim imgFile As String = IO.Path.Combine(My.Computer.FileSystem.SpecialDirectories.Temp, "srt_nettest_favicon.ico")
+    Dim pctPNG16 As New Bitmap(16, 16)
+    Dim pctPNG32 As New Bitmap(32, 32)
+    Dim didOK As Boolean = True
+    Dim imgHeader(3) As Byte
+    Using iStream As IO.FileStream = IO.File.OpenRead(imgFile)
+      iStream.Read(imgHeader, 0, 4)
+    End Using
+    Try
+      Select Case BitConverter.ToUInt32(imgHeader, 0)
+        Case &H10000
+          Using ico As New Icon(imgFile, 16, 16)
+            pctPNG16 = GenerateCloneImage(ico)
+          End Using
+          Using ico As New Icon(imgFile, 32, 32)
+            pctPNG32 = GenerateCloneImage(ico)
+          End Using
+        Case &H474E5089, &H38464947
+          Using ico As Image = Image.FromFile(imgFile)
+            pctPNG16 = GenerateCloneImage(ico, 16, 16)
+            pctPNG32 = GenerateCloneImage(ico, 32, 32)
+          End Using
+        Case Else
+          Debug.Print("Unknown Header ID: " & Hex(BitConverter.ToUInt32(imgHeader, 0)))
+          Using ico As Image = Image.FromFile(imgFile)
+            pctPNG16 = GenerateCloneImage(ico, 16, 16)
+            pctPNG32 = GenerateCloneImage(ico, 32, 32)
+          End Using
+      End Select
+    Catch ex As Exception
+      didOK = False
+    End Try
+    If IO.File.Exists(imgFile) Then IO.File.Delete(imgFile)
+    If didOK Then
+      RaiseEvent DownloadIconCompleted(Me, New DownloadIconCompletedEventArgs(pctPNG16.Clone, pctPNG32.Clone))
     Else
-      Dim imgFile As String = IO.Path.Combine(My.Computer.FileSystem.SpecialDirectories.Temp, "srt_nettest_favicon.ico")
-      Dim pctPNG16 As New Bitmap(16, 16)
-      Dim pctPNG32 As New Bitmap(32, 32)
-      Dim didOK As Boolean = True
-      Dim imgHeader(3) As Byte
-      Using iStream As IO.FileStream = IO.File.OpenRead(imgFile)
-        iStream.Read(imgHeader, 0, 4)
-      End Using
-      Try
-        Select Case BitConverter.ToUInt32(imgHeader, 0)
-          Case &H10000
-            Using ico As New Icon(imgFile, 16, 16)
-              pctPNG16 = GenerateCloneImage(ico)
-            End Using
-            Using ico As New Icon(imgFile, 32, 32)
-              pctPNG32 = GenerateCloneImage(ico)
-            End Using
-          Case &H474E5089, &H38464947
-            Using ico As Image = Image.FromFile(imgFile)
-              pctPNG16 = GenerateCloneImage(ico, 16, 16)
-              pctPNG32 = GenerateCloneImage(ico, 32, 32)
-            End Using
-          Case Else
-            Debug.Print("Unknown Header ID: " & Hex(BitConverter.ToUInt32(imgHeader, 0)))
-            Using ico As Image = Image.FromFile(imgFile)
-              pctPNG16 = GenerateCloneImage(ico, 16, 16)
-              pctPNG32 = GenerateCloneImage(ico, 32, 32)
-            End Using
-        End Select
-      Catch ex As Exception
-        didOK = False
-      End Try
-      If IO.File.Exists(imgFile) Then IO.File.Delete(imgFile)
-      If didOK Then
-        RaiseEvent DownloadIconCompleted(Me, New DownloadIconCompletedEventArgs(pctPNG16.Clone, pctPNG32.Clone))
+      If e.UserState Is Nothing Then
+        RaiseEvent DownloadIconCompleted(Me, New DownloadIconCompletedEventArgs(Nothing, Nothing, New Exception("Failed to read the icon.")))
       Else
-        If e.UserState Is Nothing Then
-          RaiseEvent DownloadIconCompleted(Me, New DownloadIconCompletedEventArgs(Nothing, Nothing, New Exception("Failed to read the icon.")))
-        Else
-          Dim pathURL As New Uri(e.UserState)
-          ConnectToFile(New Uri(pathURL.Scheme & "://" & pathURL.Host & "/favicon.ico"), imgFile)
-        End If
+        Dim pathURL As New Uri(e.UserState)
+        ConnectToFile(New Uri(pathURL.Scheme & "://" & pathURL.Host & "/favicon.ico"), imgFile)
       End If
-      If pctPNG16 IsNot Nothing Then
-        pctPNG16.Dispose()
-        pctPNG16 = Nothing
-      End If
-      If pctPNG32 IsNot Nothing Then
-        pctPNG32.Dispose()
-        pctPNG32 = Nothing
-      End If
+    End If
+    If pctPNG16 IsNot Nothing Then
+      pctPNG16.Dispose()
+      pctPNG16 = Nothing
+    End If
+    If pctPNG32 IsNot Nothing Then
+      pctPNG32.Dispose()
+      pctPNG32 = Nothing
     End If
   End Sub
   Private Sub wsNetTest_DownloadStringCompleted(sender As Object, e As System.Net.DownloadStringCompletedEventArgs) Handles wsNetTest.DownloadStringCompleted
     If e.Error IsNot Nothing Then
-      Dim pathURL As New Uri(e.UserState)
-      ConnectToFile(New Uri(pathURL.Scheme & "://" & pathURL.Host & "/favicon.ico"), IO.Path.Combine(My.Computer.FileSystem.SpecialDirectories.Temp, "srt_nettest_favicon.ico"))
+      Dim defPathURL As New Uri(e.UserState)
+      ConnectToFile(New Uri(defPathURL.Scheme & "://" & defPathURL.Host & "/favicon.ico"), IO.Path.Combine(My.Computer.FileSystem.SpecialDirectories.Temp, "srt_nettest_favicon.ico"))
+      Return
     ElseIf e.Cancelled Then
       Return
-    Else
-      Try
-        Dim sHTML As String = e.Result
-        If sHTML.ToLower.Contains("shortcut icon") Then sHTML = Replace(sHTML, "shortcut icon", "icon", , , CompareMethod.Text)
-        If sHTML.ToLower.Contains("rel=""icon""") Then
-          If sHTML.Substring(0, sHTML.ToLower.IndexOf("rel=""icon""")).Contains("<") Then
-            sHTML = sHTML.Substring(sHTML.Substring(0, sHTML.ToLower.IndexOf("rel=""icon""")).LastIndexOf("<"))
-            If sHTML.Contains(">") Then
-              sHTML = sHTML.Substring(0, sHTML.IndexOf(">") + 1)
-              If sHTML.ToLower.Contains("href") Then
-                sHTML = sHTML.Substring(sHTML.IndexOf("href"))
+    End If
+    Try
+      Dim sHTML As String = e.Result
+      If sHTML.ToLower.Contains("shortcut icon") Then sHTML = Replace(sHTML, "shortcut icon", "icon", , , CompareMethod.Text)
+      If sHTML.ToLower.Contains("rel=""icon""") Then
+        If sHTML.Substring(0, sHTML.ToLower.IndexOf("rel=""icon""")).Contains("<") Then
+          sHTML = sHTML.Substring(sHTML.Substring(0, sHTML.ToLower.IndexOf("rel=""icon""")).LastIndexOf("<"))
+          If sHTML.Contains(">") Then
+            sHTML = sHTML.Substring(0, sHTML.IndexOf(">") + 1)
+            If sHTML.ToLower.Contains("href") Then
+              sHTML = sHTML.Substring(sHTML.IndexOf("href"))
+              If sHTML.Contains("""") Then
+                sHTML = sHTML.Substring(sHTML.IndexOf("""") + 1)
                 If sHTML.Contains("""") Then
-                  sHTML = sHTML.Substring(sHTML.IndexOf("""") + 1)
-                  If sHTML.Contains("""") Then
-                    Dim URL As String = sHTML.Substring(0, sHTML.IndexOf(""""))
-                    If URL.Contains("://") Then
+                  Dim URL As String = sHTML.Substring(0, sHTML.IndexOf(""""))
+                  If URL.Contains("://") Then
 
-                    ElseIf URL.Contains("//") Then
-                      Dim oldURL As String = e.UserState
-                      If oldURL.Contains("://") Then oldURL = oldURL.Substring(0, oldURL.IndexOf("://") + 1)
+                  ElseIf URL.Contains("//") Then
+                    Dim oldURL As String = e.UserState
+                    If oldURL.Contains("://") Then oldURL = oldURL.Substring(0, oldURL.IndexOf("://") + 1)
+                    URL = oldURL & URL
+                  Else
+                    Dim oldURL As String = e.UserState
+                    If Not oldURL.EndsWith("/") And oldURL.IndexOf("/", oldURL.IndexOf("//") + 2) > -1 Then oldURL = oldURL.Substring(0, oldURL.LastIndexOf("/") + 1)
+                    If URL.StartsWith("/") Then
+                      If oldURL.IndexOf("/", oldURL.IndexOf("//") + 2) > -1 Then oldURL = oldURL.Substring(0, oldURL.IndexOf("/", oldURL.IndexOf("//") + 2))
                       URL = oldURL & URL
                     Else
-                      Dim oldURL As String = e.UserState
-                      If Not oldURL.EndsWith("/") And oldURL.IndexOf("/", oldURL.IndexOf("//") + 2) > -1 Then oldURL = oldURL.Substring(0, oldURL.LastIndexOf("/") + 1)
-                      If URL.StartsWith("/") Then
-                        If oldURL.IndexOf("/", oldURL.IndexOf("//") + 2) > -1 Then oldURL = oldURL.Substring(0, oldURL.IndexOf("/", oldURL.IndexOf("//") + 2))
-                        URL = oldURL & URL
-                      Else
-                        URL = oldURL & URL
-                      End If
+                      URL = oldURL & URL
                     End If
-                    ConnectToFile(New Uri(URL), IO.Path.Combine(My.Computer.FileSystem.SpecialDirectories.Temp, "srt_nettest_favicon.ico"), URL)
-                    Return
                   End If
+                  ConnectToFile(New Uri(URL), IO.Path.Combine(My.Computer.FileSystem.SpecialDirectories.Temp, "srt_nettest_favicon.ico"), URL)
+                  Return
                 End If
               End If
             End If
           End If
         End If
-      Catch ex As Exception
-      End Try
-      Dim pathURL As New Uri(e.UserState)
-      ConnectToFile(New Uri(pathURL.Scheme & "://" & pathURL.Host & "/favicon.ico"), IO.Path.Combine(My.Computer.FileSystem.SpecialDirectories.Temp, "srt_nettest_favicon.ico"))
-    End If
+      End If
+    Catch ex As Exception
+    End Try
+    Dim pathURL As New Uri(e.UserState)
+    ConnectToFile(New Uri(pathURL.Scheme & "://" & pathURL.Host & "/favicon.ico"), IO.Path.Combine(My.Computer.FileSystem.SpecialDirectories.Temp, "srt_nettest_favicon.ico"))
   End Sub
 #Region "IDisposable Support"
   Private disposedValue As Boolean
@@ -183,8 +177,6 @@
           wsNetTest.Dispose()
           wsNetTest = Nothing
         End If
-        c_icon16 = Nothing
-        c_icon32 = Nothing
       End If
     End If
     Me.disposedValue = True
