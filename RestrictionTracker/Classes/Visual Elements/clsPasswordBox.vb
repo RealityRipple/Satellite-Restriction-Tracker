b9cf65eac68b2b2cf2c03872745bcfe17a70fe50
Andy <webmaster@RealityRipple.com>
2015-05-13 23:42:24 -0700

    Added support for standard themes for Windows XP and Windows 8 to PasswordBox border drawing code. Added textbox width setting code in an attempt to fix large fonts.

--- RestrictionTracker/Classes/Visual Elements/clsPasswordBox.vb
+++ RestrictionTracker/Classes/Visual Elements/clsPasswordBox.vb
@@ -58,122 +58,209 @@
 
   Protected Overrides Sub OnPaint(e As System.Windows.Forms.PaintEventArgs)
     MyBase.OnPaint(e)
-
-
     Using g As Graphics = e.Graphics
-      If Environment.OSVersion.Version.Major > 5 Then
-        Dim rBox As Rectangle = Me.DisplayRectangle
-        Dim topBar, leftBar, rightBar, bottomBar As Pen
-        Dim topBar_t1, topBar_t2, topBar_t3, bottomBar_t1 As SolidBrush
-
+      If Environment.OSVersion.Version.Major = 5 Then
         If Not txtPasswordBox.Enabled Then
-          Dim border As New Pen(Color.FromArgb(175, 175, 175))
-          Dim outCorner As New SolidBrush(Color.FromArgb(120, border.Color))
-          Dim inCorner As New SolidBrush(Color.FromArgb(70, border.Color))
-          Dim inBorder As New Pen(Color.FromArgb(255, 255, 255))
-          Dim inBG As New SolidBrush(Color.FromArgb(240, 240, 240))
-          g.FillRectangle(New SolidBrush(Color.Red), rBox.Left + 1, rBox.Top + 1, rBox.Width - 2, rBox.Height - 2)
-          g.DrawLine(border, rBox.Left + 1, rBox.Top + 0, rBox.Right - 2, rBox.Top + 0)
-          g.DrawLine(inBorder, rBox.Left + 2, rBox.Top + 1, rBox.Right - 3, rBox.Top + 1)
-          g.FillRectangle(outCorner, rBox.Left, rBox.Top, 1, 1)
-          g.FillRectangle(outCorner, rBox.Right - 1, rBox.Top, 1, 1)
-
-          g.DrawLine(border, rBox.Left + 1, rBox.Bottom - 1, rBox.Right - 2, rBox.Bottom - 1)
-          g.DrawLine(inBorder, rBox.Left + 1, rBox.Bottom - 2, rBox.Right - 2, rBox.Bottom - 2)
-          g.FillRectangle(outCorner, rBox.Left, rBox.Bottom - 1, 1, 1)
-          g.FillRectangle(outCorner, rBox.Right - 1, rBox.Bottom - 1, 1, 1)
-
-          g.DrawLine(border, rBox.Left, rBox.Top + 1, rBox.Left, rBox.Bottom - 2)
-          g.DrawLine(inBorder, rBox.Left + 1, rBox.Top + 1, rBox.Left + 1, rBox.Bottom - 2)
-          g.FillRectangle(inCorner, rBox.Left + 1, rBox.Top + 1, 1, 1)
-          g.FillRectangle(inCorner, rBox.Left + 1, rBox.Bottom - 2, 1, 1)
-
-
-          g.DrawLine(border, rBox.Right - 1, rBox.Top + 1, rBox.Right - 1, rBox.Bottom - 2)
-          g.DrawLine(inBorder, rBox.Right - 2, rBox.Top + 1, rBox.Right - 2, rBox.Bottom - 2)
-          g.FillRectangle(inCorner, rBox.Right - 2, rBox.Top + 1, 1, 1)
-          g.FillRectangle(inCorner, rBox.Right - 2, rBox.Bottom - 2, 1, 1)
-
-          g.FillRectangle(inBG, rBox.Left + 2, rBox.Top + 2, rBox.Width - 4, rBox.Height - 4)
-          Return
+          DrawTextbox_XP(g, Me.DisplayRectangle, ActiveState.Disable)
         ElseIf txtPasswordBox.Focused Or pctPasswordEye.Focused Then
-          topBar = New Pen(Color.FromArgb(61, 123, 173))
-          topBar_t1 = New SolidBrush(Color.FromArgb(200, topBar.Color))
-          topBar_t2 = New SolidBrush(Color.FromArgb(120, topBar.Color))
-          topBar_t3 = New SolidBrush(Color.FromArgb(70, topBar.Color))
-
-          leftBar = New Pen(Color.FromArgb(181, 207, 231))
-          rightBar = New Pen(Color.FromArgb(164, 201, 227))
-
-          bottomBar = New Pen(Color.FromArgb(183, 217, 237))
-          bottomBar_t1 = New SolidBrush(Color.FromArgb(120, bottomBar.Color))
-
+          DrawTextbox_XP(g, Me.DisplayRectangle, ActiveState.Focus)
         ElseIf c_Hover Then
-          topBar = New Pen(Color.FromArgb(87, 148, 191))
-          topBar_t1 = New SolidBrush(Color.FromArgb(200, topBar.Color))
-          topBar_t2 = New SolidBrush(Color.FromArgb(120, topBar.Color))
-          topBar_t3 = New SolidBrush(Color.FromArgb(70, topBar.Color))
-
-          leftBar = New Pen(Color.FromArgb(197, 218, 237))
-          rightBar = New Pen(Color.FromArgb(183, 213, 234))
-
-          bottomBar = New Pen(Color.FromArgb(199, 226, 241))
-          bottomBar_t1 = New SolidBrush(Color.FromArgb(120, bottomBar.Color))
+          DrawTextbox_XP(g, Me.DisplayRectangle, ActiveState.Hover)
         Else
-          topBar = New Pen(Color.FromArgb(171, 173, 179))
-          topBar_t1 = New SolidBrush(Color.FromArgb(200, topBar.Color))
-          topBar_t2 = New SolidBrush(Color.FromArgb(120, topBar.Color))
-          topBar_t3 = New SolidBrush(Color.FromArgb(70, topBar.Color))
-
-          leftBar = New Pen(Color.FromArgb(226, 227, 234))
-          rightBar = New Pen(Color.FromArgb(219, 223, 230))
-
-          bottomBar = New Pen(Color.FromArgb(227, 233, 239))
-          bottomBar_t1 = New SolidBrush(Color.FromArgb(120, bottomBar.Color))
-
+          DrawTextbox_XP(g, Me.DisplayRectangle, ActiveState.Normal)
         End If
-        g.FillRectangle(New SolidBrush(SystemColors.Window), rBox.Left + 1, rBox.Top + 1, rBox.Width - 2, rBox.Height - 2)
-        g.FillRectangle(topBar_t2, rBox.Left, rBox.Top, 1, 1)
-        g.FillRectangle(topBar_t1, rBox.Left + 1, rBox.Top, 1, 1)
-        g.DrawLine(topBar, rBox.Left + 2, rBox.Top + 0, rBox.Right - 2, rBox.Top + 0)
-        g.FillRectangle(topBar_t1, rBox.Right - 2, rBox.Top, 1, 1)
-        g.FillRectangle(topBar_t2, rBox.Right - 1, rBox.Top, 1, 1)
-
-        g.FillRectangle(topBar_t3, rBox.Left + 1, rBox.Top + 1, 1, 1)
-        g.DrawLine(leftBar, rBox.Left, rBox.Top + 1, rBox.Left, rBox.Bottom - 2)
-        g.FillRectangle(topBar_t3, rBox.Left + 1, rBox.Bottom - 2, 1, 1)
-
-        g.FillRectangle(topBar_t3, rBox.Right - 2, rBox.Top + 1, 1, 1)
-        g.DrawLine(rightBar, rBox.Right - 1, rBox.Top + 1, rBox.Right - 1, rBox.Bottom - 2)
-        g.FillRectangle(topBar_t3, rBox.Right - 2, rBox.Bottom - 2, 1, 1)
-
-        g.FillRectangle(bottomBar_t1, rBox.Left, rBox.Bottom - 1, 1, 1)
-        g.DrawLine(bottomBar, rBox.Left + 1, rBox.Bottom - 1, rBox.Right - 2, rBox.Bottom - 1)
-        g.FillRectangle(bottomBar_t1, rBox.Right - 1, rBox.Bottom - 1, 1, 1)
-      Else
-        If VisualStyles.VisualStyleRenderer.IsSupported Then
+      ElseIf Environment.OSVersion.Version.Major = 6 Then
+        If Environment.OSVersion.Version.Minor < 2 Then
           If Not txtPasswordBox.Enabled Then
-            Dim r As New VisualStyles.VisualStyleRenderer(VisualStyles.VisualStyleElement.TextBox.TextEdit.Disabled)
-            r.DrawBackground(g, Me.ClientRectangle, e.ClipRectangle)
-            r.DrawEdge(g, Me.ClientRectangle, VisualStyles.Edges.Top Or VisualStyles.Edges.Left Or VisualStyles.Edges.Bottom Or VisualStyles.Edges.Right, VisualStyles.EdgeStyle.Etched, VisualStyles.EdgeEffects.None)
+            DrawTextbox_V7(g, Me.DisplayRectangle, ActiveState.Disable)
           ElseIf txtPasswordBox.Focused Or pctPasswordEye.Focused Then
-            Dim r As New VisualStyles.VisualStyleRenderer(VisualStyles.VisualStyleElement.TextBox.TextEdit.Focused)
-            r.DrawBackground(g, Me.ClientRectangle, e.ClipRectangle)
-            r.DrawEdge(g, Me.ClientRectangle, VisualStyles.Edges.Top Or VisualStyles.Edges.Left Or VisualStyles.Edges.Bottom Or VisualStyles.Edges.Right, VisualStyles.EdgeStyle.Etched, VisualStyles.EdgeEffects.Flat)
+            DrawTextbox_V7(g, Me.DisplayRectangle, ActiveState.Focus)
           ElseIf c_Hover Then
-            Dim r As New VisualStyles.VisualStyleRenderer(VisualStyles.VisualStyleElement.TextBox.TextEdit.Hot)
-            r.DrawBackground(g, Me.ClientRectangle, e.ClipRectangle)
-            r.DrawEdge(g, Me.ClientRectangle, VisualStyles.Edges.Top Or VisualStyles.Edges.Left Or VisualStyles.Edges.Bottom Or VisualStyles.Edges.Right, VisualStyles.EdgeStyle.Etched, VisualStyles.EdgeEffects.Flat)
+            DrawTextbox_V7(g, Me.DisplayRectangle, ActiveState.Hover)
           Else
-            Dim r As New VisualStyles.VisualStyleRenderer(VisualStyles.VisualStyleElement.TextBox.TextEdit.Normal)
-            r.DrawBackground(g, Me.ClientRectangle, e.ClipRectangle)
-            r.DrawEdge(g, Me.ClientRectangle, VisualStyles.Edges.Top Or VisualStyles.Edges.Left Or VisualStyles.Edges.Bottom Or VisualStyles.Edges.Right, VisualStyles.EdgeStyle.Etched, VisualStyles.EdgeEffects.Flat)
+            DrawTextbox_V7(g, Me.DisplayRectangle, ActiveState.Normal)
           End If
         Else
-
+          If Not txtPasswordBox.Enabled Then
+            DrawTextbox_8(g, Me.DisplayRectangle, ActiveState.Disable)
+          ElseIf txtPasswordBox.Focused Or pctPasswordEye.Focused Then
+            DrawTextbox_8(g, Me.DisplayRectangle, ActiveState.Focus)
+          ElseIf c_Hover Then
+            DrawTextbox_8(g, Me.DisplayRectangle, ActiveState.Hover)
+          Else
+            DrawTextbox_8(g, Me.DisplayRectangle, ActiveState.Normal)
+          End If
+        End If
+      ElseIf Environment.OSVersion.Version.Major = 10 Then
+        If Not txtPasswordBox.Enabled Then
+          DrawTextbox_8(g, Me.DisplayRectangle, ActiveState.Disable)
+        ElseIf txtPasswordBox.Focused Or pctPasswordEye.Focused Then
+          DrawTextbox_8(g, Me.DisplayRectangle, ActiveState.Focus)
+        ElseIf c_Hover Then
+          DrawTextbox_8(g, Me.DisplayRectangle, ActiveState.Hover)
+        Else
+          DrawTextbox_8(g, Me.DisplayRectangle, ActiveState.Normal)
+        End If
+      Else
+        If Not txtPasswordBox.Enabled Then
+          DrawTextbox(g, Me.DisplayRectangle, ActiveState.Disable)
+        ElseIf txtPasswordBox.Focused Or pctPasswordEye.Focused Then
+          DrawTextbox(g, Me.DisplayRectangle, ActiveState.Focus)
+        ElseIf c_Hover Then
+          DrawTextbox(g, Me.DisplayRectangle, ActiveState.Hover)
+        Else
+          DrawTextbox(g, Me.DisplayRectangle, ActiveState.Normal)
         End If
       End If
     End Using
+  End Sub
+
+  Private Enum ActiveState
+    Normal
+    Hover
+    Focus
+    Disable
+  End Enum
+
+  Private Sub DrawTextbox_XP(ByRef g As Graphics, drawBox As Rectangle, state As ActiveState)
+    Dim borderColor As Pen
+    Dim bgColor As SolidBrush
+    Select Case state
+      Case ActiveState.Normal
+        borderColor = New Pen(Color.FromArgb(127, 157, 185))
+        bgColor = Brushes.White
+      Case ActiveState.Hover
+        borderColor = New Pen(Color.FromArgb(127, 157, 185))
+        bgColor = Brushes.White
+      Case ActiveState.Focus
+        borderColor = New Pen(Color.FromArgb(127, 157, 185))
+        bgColor = Brushes.White
+      Case Else
+        borderColor = New Pen(Color.FromArgb(126, 155, 183))
+        bgColor = New SolidBrush(Color.FromArgb(236, 233, 216))
+    End Select
+    g.FillRectangle(bgColor, drawBox.Left + 1, drawBox.Top + 1, drawBox.Width - 2, drawBox.Height - 2)
+    g.DrawRectangle(borderColor, drawBox.Left, drawBox.Top, drawBox.Width - 1, drawBox.Height - 1)
+  End Sub
+
+  Private Sub DrawTextbox_V7(ByRef g As Graphics, drawBox As Rectangle, state As ActiveState)
+    Dim topBar, leftBar, rightBar, bottomBar As Pen
+    Dim topBar_t1, topBar_t2, topBar_t3, bottomBar_t1 As SolidBrush
+    Select Case state
+      Case ActiveState.Normal
+        topBar = New Pen(Color.FromArgb(171, 173, 179))
+        topBar_t1 = New SolidBrush(Color.FromArgb(200, topBar.Color))
+        topBar_t2 = New SolidBrush(Color.FromArgb(120, topBar.Color))
+        topBar_t3 = New SolidBrush(Color.FromArgb(70, topBar.Color))
+        leftBar = New Pen(Color.FromArgb(226, 227, 234))
+        rightBar = New Pen(Color.FromArgb(219, 223, 230))
+        bottomBar = New Pen(Color.FromArgb(227, 233, 239))
+        bottomBar_t1 = New SolidBrush(Color.FromArgb(120, bottomBar.Color))
+      Case ActiveState.Hover
+        topBar = New Pen(Color.FromArgb(87, 148, 191))
+        topBar_t1 = New SolidBrush(Color.FromArgb(200, topBar.Color))
+        topBar_t2 = New SolidBrush(Color.FromArgb(120, topBar.Color))
+        topBar_t3 = New SolidBrush(Color.FromArgb(70, topBar.Color))
+        leftBar = New Pen(Color.FromArgb(197, 218, 237))
+        rightBar = New Pen(Color.FromArgb(183, 213, 234))
+        bottomBar = New Pen(Color.FromArgb(199, 226, 241))
+        bottomBar_t1 = New SolidBrush(Color.FromArgb(120, bottomBar.Color))
+      Case ActiveState.Focus
+        topBar = New Pen(Color.FromArgb(61, 123, 173))
+        topBar_t1 = New SolidBrush(Color.FromArgb(200, topBar.Color))
+        topBar_t2 = New SolidBrush(Color.FromArgb(120, topBar.Color))
+        topBar_t3 = New SolidBrush(Color.FromArgb(70, topBar.Color))
+        leftBar = New Pen(Color.FromArgb(181, 207, 231))
+        rightBar = New Pen(Color.FromArgb(164, 201, 227))
+        bottomBar = New Pen(Color.FromArgb(183, 217, 237))
+        bottomBar_t1 = New SolidBrush(Color.FromArgb(120, bottomBar.Color))
+      Case Else
+        Dim border As New Pen(Color.FromArgb(175, 175, 175))
+        Dim outCorner As New SolidBrush(Color.FromArgb(120, border.Color))
+        Dim inCorner As New SolidBrush(Color.FromArgb(70, border.Color))
+        Dim inBorder As New Pen(Color.FromArgb(255, 255, 255))
+        Dim inBG As New SolidBrush(Color.FromArgb(240, 240, 240))
+        g.FillRectangle(New SolidBrush(Color.Red), drawBox.Left + 1, drawBox.Top + 1, drawBox.Width - 2, drawBox.Height - 2)
+        g.DrawLine(border, drawBox.Left + 1, drawBox.Top + 0, drawBox.Right - 2, drawBox.Top + 0)
+        g.DrawLine(inBorder, drawBox.Left + 2, drawBox.Top + 1, drawBox.Right - 3, drawBox.Top + 1)
+        g.FillRectangle(outCorner, drawBox.Left, drawBox.Top, 1, 1)
+        g.FillRectangle(outCorner, drawBox.Right - 1, drawBox.Top, 1, 1)
+        g.DrawLine(border, drawBox.Left + 1, drawBox.Bottom - 1, drawBox.Right - 2, drawBox.Bottom - 1)
+        g.DrawLine(inBorder, drawBox.Left + 1, drawBox.Bottom - 2, drawBox.Right - 2, drawBox.Bottom - 2)
+        g.FillRectangle(outCorner, drawBox.Left, drawBox.Bottom - 1, 1, 1)
+        g.FillRectangle(outCorner, drawBox.Right - 1, drawBox.Bottom - 1, 1, 1)
+        g.DrawLine(border, drawBox.Left, drawBox.Top + 1, drawBox.Left, drawBox.Bottom - 2)
+        g.DrawLine(inBorder, drawBox.Left + 1, drawBox.Top + 1, drawBox.Left + 1, drawBox.Bottom - 2)
+        g.FillRectangle(inCorner, drawBox.Left + 1, drawBox.Top + 1, 1, 1)
+        g.FillRectangle(inCorner, drawBox.Left + 1, drawBox.Bottom - 2, 1, 1)
+        g.DrawLine(border, drawBox.Right - 1, drawBox.Top + 1, drawBox.Right - 1, drawBox.Bottom - 2)
+        g.DrawLine(inBorder, drawBox.Right - 2, drawBox.Top + 1, drawBox.Right - 2, drawBox.Bottom - 2)
+        g.FillRectangle(inCorner, drawBox.Right - 2, drawBox.Top + 1, 1, 1)
+        g.FillRectangle(inCorner, drawBox.Right - 2, drawBox.Bottom - 2, 1, 1)
+        g.FillRectangle(inBG, drawBox.Left + 2, drawBox.Top + 2, drawBox.Width - 4, drawBox.Height - 4)
+        Return
+    End Select
+    g.FillRectangle(Brushes.White, drawBox.Left + 1, drawBox.Top + 1, drawBox.Width - 2, drawBox.Height - 2)
+    g.FillRectangle(topBar_t2, drawBox.Left, drawBox.Top, 1, 1)
+    g.FillRectangle(topBar_t1, drawBox.Left + 1, drawBox.Top, 1, 1)
+    g.DrawLine(topBar, drawBox.Left + 2, drawBox.Top + 0, drawBox.Right - 2, drawBox.Top + 0)
+    g.FillRectangle(topBar_t1, drawBox.Right - 2, drawBox.Top, 1, 1)
+    g.FillRectangle(topBar_t2, drawBox.Right - 1, drawBox.Top, 1, 1)
+    g.FillRectangle(topBar_t3, drawBox.Left + 1, drawBox.Top + 1, 1, 1)
+    g.DrawLine(leftBar, drawBox.Left, drawBox.Top + 1, drawBox.Left, drawBox.Bottom - 2)
+    g.FillRectangle(topBar_t3, drawBox.Left + 1, drawBox.Bottom - 2, 1, 1)
+    g.FillRectangle(topBar_t3, drawBox.Right - 2, drawBox.Top + 1, 1, 1)
+    g.DrawLine(rightBar, drawBox.Right - 1, drawBox.Top + 1, drawBox.Right - 1, drawBox.Bottom - 2)
+    g.FillRectangle(topBar_t3, drawBox.Right - 2, drawBox.Bottom - 2, 1, 1)
+    g.FillRectangle(bottomBar_t1, drawBox.Left, drawBox.Bottom - 1, 1, 1)
+    g.DrawLine(bottomBar, drawBox.Left + 1, drawBox.Bottom - 1, drawBox.Right - 2, drawBox.Bottom - 1)
+    g.FillRectangle(bottomBar_t1, drawBox.Right - 1, drawBox.Bottom - 1, 1, 1)
+  End Sub
+
+  Private Sub DrawTextbox_8(ByRef g As Graphics, drawBox As Rectangle, state As ActiveState)
+    Dim borderColor As Pen
+    Dim bgColor As SolidBrush
+    Select Case state
+      Case ActiveState.Normal
+        borderColor = New Pen(Color.FromArgb(171, 173, 179))
+        bgColor = Brushes.White
+      Case ActiveState.Hover
+        borderColor = New Pen(Color.FromArgb(126, 180, 234))
+        bgColor = Brushes.White
+      Case ActiveState.Focus
+        borderColor = New Pen(Color.FromArgb(86, 157, 229))
+        bgColor = Brushes.White
+      Case Else
+        borderColor = New Pen(Color.FromArgb(217, 217, 217))
+        bgColor = New SolidBrush(Color.FromArgb(240, 240, 240))
+        g.FillRectangle(Brushes.White, drawBox.Left + 1, drawBox.Top + 1, drawBox.Width - 2, drawBox.Height - 2)
+        g.FillRectangle(bgColor, drawBox.Left + 2, drawBox.Top + 2, drawBox.Width - 4, drawBox.Height - 4)
+        g.DrawRectangle(borderColor, drawBox.Left, drawBox.Top, drawBox.Width - 1, drawBox.Height - 1)
+        Return
+    End Select
+    g.FillRectangle(bgColor, drawBox.Left + 1, drawBox.Top + 1, drawBox.Width - 2, drawBox.Height - 2)
+    g.DrawRectangle(borderColor, drawBox.Left, drawBox.Top, drawBox.Width - 1, drawBox.Height - 1)
+  End Sub
+
+  Private Sub DrawTextbox(ByRef g As Graphics, drawBox As Rectangle, state As ActiveState)
+    Dim borderColor As Pen
+    Dim bgColor As SolidBrush
+    Select Case state
+      Case ActiveState.Normal
+        borderColor = New Pen(SystemColors.InactiveBorder)
+        bgColor = New SolidBrush(SystemColors.Window)
+      Case ActiveState.Hover
+        borderColor = New Pen(SystemColors.ActiveBorder)
+        bgColor = New SolidBrush(SystemColors.Window)
+      Case ActiveState.Focus
+        borderColor = New Pen(SystemColors.ActiveBorder)
+        bgColor = New SolidBrush(SystemColors.Window)
+      Case Else
+        borderColor = New Pen(SystemColors.ButtonFace)
+        bgColor = New SolidBrush(SystemColors.ButtonShadow)
+    End Select
+    g.FillRectangle(bgColor, drawBox.Left + 1, drawBox.Top + 1, drawBox.Width - 2, drawBox.Height - 2)
+    g.DrawRectangle(borderColor, drawBox.Left, drawBox.Top, drawBox.Width - 1, drawBox.Height - 1)
   End Sub
 
   Private Sub txtPasswordBox_GotFocus(sender As Object, e As System.EventArgs) Handles txtPasswordBox.GotFocus
@@ -210,5 +297,13 @@
   Private Sub PasswordBox_MouseLeave(sender As Object, e As System.EventArgs) Handles Me.MouseLeave
     c_Hover = False
     Me.Invalidate()
+  End Sub
+
+  Private Sub PasswordBox_SizeChanged(sender As Object, e As System.EventArgs) Handles Me.SizeChanged
+    If pctPasswordEye.Visible Then
+      txtPasswordBox.Width = Me.DisplayRectangle.Width - pctPasswordEye.Width - 4
+    Else
+      txtPasswordBox.Width = Me.DisplayRectangle.Width - 2
+    End If
   End Sub
 End Class
