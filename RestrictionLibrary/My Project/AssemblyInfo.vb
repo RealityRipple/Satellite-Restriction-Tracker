7591a53fadf45442cad31521d5a123e79b5e69ef
Andrew Sachen <webmaster@RealityRipple.com>
2017-03-05 16:03:12 -0800

    Upped version numbers.

--- RestrictionLibrary/My Project/AssemblyInfo.vb
+++ RestrictionLibrary/My Project/AssemblyInfo.vb
@@ -31,6 +31,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.3.8")> 
-<Assembly: AssemblyFileVersion("1.3.8")> 
+<Assembly: AssemblyVersion("1.3.8.1")> 
+<Assembly: AssemblyFileVersion("1.3.8.1")> 
 
