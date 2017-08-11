65e07da6bd9b42a6cccb248790ac9ce1af24feaf
Andy <webmaster@RealityRipple.com>
2017-08-10 21:29:06 -0700

    Increment Version Numbers

--- RestrictionLibrary/My Project/AssemblyInfo.vb
+++ RestrictionLibrary/My Project/AssemblyInfo.vb
@@ -31,6 +31,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.4.17")> 
-<Assembly: AssemblyFileVersion("1.4.17")> 
+<Assembly: AssemblyVersion("1.4.171")> 
+<Assembly: AssemblyFileVersion("1.4.171")> 
 
--- RestrictionLogger/My Project/AssemblyInfo.vb
+++ RestrictionLogger/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.4.17")> 
-<Assembly: AssemblyFileVersion("1.4.17")> 
+<Assembly: AssemblyVersion("1.4.171")> 
+<Assembly: AssemblyFileVersion("1.4.171")> 
