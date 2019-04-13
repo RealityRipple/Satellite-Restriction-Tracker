72b439f1e6ca196e6d0ce7f622203b9a8967d946
Andy <webmaster@RealityRipple.com>
2019-04-13 10:25:26 -0700

    Increment Version Numbers

--- RestrictionLibrary/My Project/AssemblyInfo.vb
+++ RestrictionLibrary/My Project/AssemblyInfo.vb
@@ -31,6 +31,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.5.4")> 
-<Assembly: AssemblyFileVersion("1.5.4")> 
+<Assembly: AssemblyVersion("1.5.4.1")> 
+<Assembly: AssemblyFileVersion("1.5.4.1")> 
 
